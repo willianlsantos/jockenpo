49a7c69e678f3e84f84deb681de7f5a12a7a2428
Willian <williansantos.developer@gmail.com>
2023-11-13 19:16:18 +0000

    projeto jo ken po

---
+++ script.js
@@ -0,0 +1,37 @@
+const result = document.querySelector(`.result`)
+const humanScore = document.querySelector(`#human-score`)
+const machineScore = document.querySelector(`#machine-score`)
+
+let humanScoreNumber = 0
+let machineScoreNumber = 0
+
+
+const PlayHuman = (humanChoice) => {
+
+    playTheGame(humanChoice, playMachine())
+}
+
+const playMachine = () => {
+    const choices = [`rock`, `paper`, `scissors`]
+    const randomNumber = Math.floor(Math.random() * 3)
+
+    return choices[randomNumber]
+}
+
+const playTheGame = (human, machine) => {
+    console.log(`humano: ` + human + ` maquina: ` + machine)
+
+    if (human === machine) {
+        result.innerHTML = `Deu Empate !`
+    } else if (human === `paper` && machine === `rock` || human === `rock` && machine === `scissors` || human === `scissors` && machine === `paper`) {
+        humanScoreNumber++
+        humanScore.innerHTML = humanScoreNumber
+        result.innerHTML = `Voce Ganhou :)`
+    } else {
+        machineScoreNumber++
+        machineScore.innerHTML = machineScoreNumber
+        result.innerHTML = `Voce Perdeu :(`
+    }
+
+}
+
